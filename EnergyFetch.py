23b26abd5e183605b6491ac69a6b37e2d265f9a8
Ryan <rsmith23@wolverine.cs.binghamton.edu>
2015-01-16 12:48:57 -0500

    EnergyFetch now records to the database

--- EnergyFetch.py
+++ EnergyFetch.py
@@ -14,7 +14,7 @@ def main():
    db_user = os.getenv('DB_USERNAME')
    db_pass = os.getenv('DB_PASS')
    db_name = os.getenv('DB_NAME')
-   
+  
    #Open up meter and set configure it
    meter  = serial.Serial('/dev/ttyUSB0',115200)
    meter.write("L,W,3,E,,1;")
@@ -25,12 +25,12 @@ def main():
 
       # Getting the machines id from the database
       try:
-         row = cur.execute("SELECT TOP 1 FROM machines WHERE name='%s'" % \
-                 uname()[1])
+         cur.execute("SELECT * FROM machines WHERE name='%s'" % \
+                 (uname()[1]))
+         row = cur.fetchone()
+         m_ID = row[0]
       except mdb.Error, e:
          print "Error %d: %s" % (e.args[0], e.args[1])
-
-      m_ID = row[0]
 
       for line in meter:
          if line.startswith('#d'):
@@ -39,17 +39,17 @@ def main():
             volts = float(fields[4]) / 10
             amps = float(fields[5]) / 1000
             #pf = float(fields[16]) / 100
-            cpu = psutil.cpu_percent()
-            memory = psutil.virtual_memory().percent
-            time = datetime.datetime.now()
+            cpu = float(psutil.cpu_percent(1))
+            memory = float(psutil.virtual_memory().percent)
+            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 
             # Insert query statement with io_usage at 0
             insertValues = """INSERT INTO recordings
                                 (time_stamp, watts, amps, volts, cpu_usage,
-                                mem_usage, io_usage, machineId) VALUES 
-                                (?, ?, ?, ?, ?, ?, ?)""", \
-                                (time, watts, amps, volts, cpu, memory, 0, m_ID)
-
+                                mem_usage, io_usage, machine_id) VALUES 
+                                ('%s', '%f', '%f', '%f', '%f', '%f', '%f', '%d')""" % \
+				(time, watts, amps, volts, cpu, memory, 0.0, m_ID)
+                                
             print("W: " + str(watts))
             print("V: " + str(volts))
             print("A: " + str(amps))
