ce9a1c96c7e50f41ba8189a44bcefdd8f580a723
Your Name <you@example.com>
2015-01-16 11:03:30 -0500

    First attempt at recording to the database

--- EnergyFetch.py
+++ EnergyFetch.py
@@ -3,6 +3,9 @@
 import MySQLdb as mdb
 import os
 import serial
+import psutil
+import datetime
+from platform import uname
 
 def main():
 
@@ -19,7 +22,16 @@ def main():
    try:
       con = mdb.connect(db_host, db_user, db_pass, db_name)
       cur = con.cursor()
-      
+
+   # Getting the machines id from the database
+   try:
+      row = cur.execute("SELECT TOP 1 FROM machines WHERE name='%s'" % \
+              uname()[1])
+   except: mdb.Error, e:
+      print "Error %d: %s" % (e.args[0], e.args[1])
+
+   m_ID = row[0]
+
       for line in meter:
          if line.startswith('#d'):
             fields = line.split(',')
@@ -27,11 +39,28 @@ def main():
             volts = float(fields[4]) / 10
             amps = float(fields[5]) / 1000
             #pf = float(fields[16]) / 100
+            cpu = psutil.cpu_percent()
+            memory = psutil.virtual_memory().percent
+            time = datetime.datetime.now()
+
+            # Insert query statement with io_usage at 0
+            insertValues = """INSERT INTO recordings
+                                (time_stamp, watts, amps, volts, cpu_usage,
+                                mem_usage, io_usage, machineId) VALUES 
+                                (?, ?, ?, ?, ?, ?, ?)""", \
+                                (time, watts, amps, volts, cpu, memory, 0, m_ID)
 
             print("W: " + str(watts))
             print("V: " + str(volts))
             print("A: " + str(amps))
             #print("Power Factor: " + str(pf))
+
+            # Insert recording into the database
+            try:
+               cur.execute(insertValues)
+               con.commit()
+            except mdb.Error, e:
+               print "Error %d: %s" % (e.args[0], e.args[1])
                     
    except mdb.Error, e:
       print "Error %d: %s" % (e.args[0], e.args[1])
